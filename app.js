e0cfc49e54f06574f259365034d34a653bd2c531
Kayphrosh <olakunbiolabode@yahoo.com>
2020-12-19 02:09:06 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const tl = gsap.timeline({defaults: { ease: "power1.out"} });
+
+tl.to('.text', {y: "0%", duration: 2, stagger: 0.25});
+tl.to('.slider', { y: "-100%", duration: 1, delay: 0.5});
+tl.to(".intro", {y: "-100%", duration: 1}, "-=1");
+
--- main.js
+++ main.js
@@ -12,3 +12,15 @@ hamburger.addEventListener("click", () => {
 
     
 });
+const timeTwo = gsap.timeline({defaults: { ease: "power1.out"} });
+
+timeTwo.to('.text', {y: "0%", duration: 2, stagger: 0.25});
+timeTwo.to('.slider', { y: "-100%", duration: 1.5, delay: 0.5});
+timeTwo.to(".intro", {y: "-100%", duration: 1}, "-=1");
+
+
+
+
+
+
+
